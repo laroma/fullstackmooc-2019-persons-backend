46dcc067e4e434ca72a96d2c3fc29ff9b6fdd2a3
Toni Laroma <toni.laroma@sanoma.com>
2019-04-13 12:56:59 +0300

    front-end bundle

---
+++ build/precache-manifest.4f1b656657a8c3a06db497e98c1d5e3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "532920d5478d3d90c803",
+    "url": "/static/css/main.77c54829.chunk.css"
+  },
+  {
+    "revision": "532920d5478d3d90c803",
+    "url": "/static/js/main.b3be0473.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "ed411938312f1ad2253c6c0528864db9",
+    "url": "/index.html"
+  }
+];
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ morgan.token('body-json', req => {
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(express.static('build'));
 app.use(
   morgan(
     ':method :url :status :res[content-length] - :response-time ms :body-json'
